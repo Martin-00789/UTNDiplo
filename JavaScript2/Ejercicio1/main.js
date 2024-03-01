43093c77a59804afaaca417b041c7510c250470b
Martin <you@example.com84286448+Martin-00789@users.noreply.github.com>
2024-03-01 17:54:45 -0300

    M3U4 Javascript

---
+++ JavaScript2/Ejercicio1/main.js
@@ -0,0 +1,11 @@
+document.getElementById("square1").addEventListener("click", function() {
+  document.body.style.backgroundColor = "lightblue";
+});
+
+document.getElementById("square2").addEventListener("click", function() {
+  document.body.style.backgroundColor = "lightgreen";
+});
+
+document.getElementById("square3").addEventListener("click", function() {
+  document.body.style.backgroundColor = "lightcoral";
+});
